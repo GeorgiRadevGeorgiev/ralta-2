3902ea13319e32e7be193a861ebd3f391e554454
GeorgiRadevGeorgiev <go6o_mamuta@abv.bg>
2016-10-27 19:43:35 +0300

    Add files via upload

---
+++ gosho ralta 2/gosho ralta 2.cpp
@@ -0,0 +1,45 @@
+// gosho ralta 2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{	
+	/*int a; 
+	cout << " Input number: ";
+	cin >> a;
+
+	if (a % 2 == 0)
+		cout << "\nThe number is even." << endl;
+	else if ((a) % 2 != 0)
+		cout << "\nThe number is odd." << endl;
+	*/
+
+    
+/*	int a, b;
+	char action;
+	cout << "Input number a = "; 
+	cin >> a;
+	cout << "Input number b = ";
+	cin >> b;
+	cout << "What do you want to do with the numbers? \" +, -, *, / \" ";
+	cin >> action;
+	switch (action){
+		case '+': cout << a + b << endl; break;
+		case '-': cout << a - b << endl; break;
+		case '*': cout << a*b << endl; break;
+		case '/': cout << (b == 0 ? 0 : a / b)<< endl; break;
+		default: cout << "error" << endl;
+	}
+*/
+
+
+
+	system("pause");
+
+    return 0;
+}
+
